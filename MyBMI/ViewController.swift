9936b0e005a62cbd70ce4e32ff484146e10238a2
Tran Quynh Tran <tran@trans-macbook-pro.local>
2018-05-18 12:51:20 +1200

    Implement data core and update app icons

--- MyBMI/ViewController.swift
+++ MyBMI/ViewController.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var unitsSegmentControl: UISegmentedControl!
-    @IBOutlet weak var weightUnitLabel: UILabel!
-    @IBOutlet weak var heightUnitLabel: UILabel!
     @IBOutlet weak var weightText: UITextField!
     @IBOutlet weak var heightText: UITextField!
     @IBOutlet weak var bmiResultLabel: UILabel!
     @IBOutlet weak var bmiMessageLabel: UILabel!
     @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var historyDataLabel: UILabel!
     
     var bmiData:Set<Bmi> = []
     
@@ -27,12 +27,15 @@ class ViewController: UIViewController {
     
     @IBAction func calculateBMITouchUp(_ sender: UIButton) {
         displayBMI()
+        saveToHistoryData()
+        loadHistoryData()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadAppearance()
         loadBMIData()
+        loadHistoryData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,7 +58,7 @@ class ViewController: UIViewController {
         bmiData.insert(Bmi(beginWeight: 0, endWeight: 15, bmiCategory: "Very severely underweight", bmiMessageType: BmiMessageType.warning))
         bmiData.insert(Bmi(beginWeight: 15, endWeight: 16, bmiCategory: "Severely underweight", bmiMessageType: BmiMessageType.warning))
         bmiData.insert(Bmi(beginWeight: 16, endWeight: 18.5, bmiCategory: "Underweight", bmiMessageType: BmiMessageType.warning))
-        bmiData.insert(Bmi(beginWeight: 18.5, endWeight: 25, bmiCategory: "Normal. Congratulation!", bmiMessageType: BmiMessageType.normal))
+        bmiData.insert(Bmi(beginWeight: 18.5, endWeight: 25, bmiCategory: "Normal. Congratulation", bmiMessageType: BmiMessageType.normal))
         bmiData.insert(Bmi(beginWeight: 25, endWeight: 30, bmiCategory: "Overweight", bmiMessageType: BmiMessageType.warning))
         bmiData.insert(Bmi(beginWeight: 30, endWeight: 35, bmiCategory: "Moderately obese", bmiMessageType: BmiMessageType.danger))
         bmiData.insert(Bmi(beginWeight: 35, endWeight: 40, bmiCategory: "Severely obese", bmiMessageType: BmiMessageType.danger))
@@ -110,20 +113,59 @@ class ViewController: UIViewController {
         }
     }
     
-    func loadHistoryData() {
-        /*
+    func getContextFromCoreData () -> NSManagedObjectContext {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        let entity = NSEntityDescription.entity(forEntityName: "BmiEnity", in: context)
-        let newUser = NSManagedObject(entity: entity!, insertInto: context)
-        
-        newUser.setValue(24.99, forKey: "bmi")
-        newUser.setValue(24.99, forKey: "bmi")
-        */
+        return context
     }
     
-    func saveHistoryData() {
+    func loadHistoryData() {
+        let context = getContextFromCoreData()
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BmiData")
+        request.returnsObjectsAsFaults = false
+        historyDataLabel.text! = ""
+        do {
+            let results = try context.fetch(request)
+            for index in stride(from: results.count-1, to: 0, by: -1) {
+                let item = results[index] as! NSManagedObject
+                if let createdDate = item.value(forKey: "createdDate") as? String {
+                    historyDataLabel.text! += createdDate + ": "
+                }
+                if let result = item.value(forKey: "result") as? String {
+                    historyDataLabel.text! += result + ". "
+                }
+                if let message = item.value(forKey: "message") as? String {
+                    historyDataLabel.text! += message + "."
+                }
+                historyDataLabel.text! += "\n"
+            }
+        } catch {
+            print("Request failed")
+            print("Unexpected error: \(error).")
+        }
+    }
+    
+    func saveToHistoryData() {
+        do {
+            let context = getContextFromCoreData()
+            let newValue = NSEntityDescription.insertNewObject(forEntityName: "BmiData", into: context)
+            
+            newValue.setValue(getNow(), forKey: "createdDate")
+            newValue.setValue(bmiResultLabel.text, forKey: "result")
+            newValue.setValue(bmiMessageLabel.text, forKey: "message")
         
+            try context.save()
+        } catch {
+            print("Failed to save")
+            print("Unexpected error: \(error).")
+        }
+    }
+    
+    func getNow() -> String {
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "yyyy-MM-dd"
+        let now = dateformatter.string(from: Date())
+        return now
     }
 }
 
