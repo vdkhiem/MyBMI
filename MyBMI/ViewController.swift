544c11632035151118f9ad2139de6e4c13da7b88
Tran Quynh Tran <tran@trans-mbp.hub>
2018-05-19 18:26:27 +1200

    Change history data from Label to TableView. Allow max 5 history data.

--- MyBMI/ViewController.swift
+++ MyBMI/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var unitsSegmentControl: UISegmentedControl!
     @IBOutlet weak var weightText: UITextField!
@@ -17,8 +17,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var bmiResultLabel: UILabel!
     @IBOutlet weak var bmiMessageLabel: UILabel!
     @IBOutlet weak var calculateButton: UIButton!
-    @IBOutlet weak var historyDataLabel: UILabel!
+    @IBOutlet weak var tableViewHistoryData: UITableView!
     
+    let historyDataMaxCount = 5
     var bmiData:Set<Bmi> = []
     
     @IBAction func unitsChange(_ sender: Any) {
@@ -28,14 +29,13 @@ class ViewController: UIViewController {
     @IBAction func calculateBMITouchUp(_ sender: UIButton) {
         displayBMI()
         saveToHistoryData()
-        loadHistoryData()
+        self.tableViewHistoryData.reloadData()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadAppearance()
         loadBMIData()
-        loadHistoryData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,12 +43,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let historyDataList = fetchHistoryDataList()
+        return historyDataList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "History Data Cell")
+        let historyDataList = fetchHistoryDataList()
+        let index = (historyDataList.count - 1) - indexPath.row
+        
+        if (historyDataList.count > 0) {
+            let item = historyDataList[index]
+            var value: String = ""
+            
+            if let createdDate = item.value(forKey: "createdDate") as? String {
+                value += createdDate + ": "
+            }
+            if let result = item.value(forKey: "result") as? String {
+                value += result + ". "
+            }
+            if let message = item.value(forKey: "message") as? String {
+                value += message + "."
+            }
+            cell.textLabel?.text = value + "\n"
+            cell.textLabel?.font = UIFont(name:"Avenir", size:12)
+        }
+        
+        return cell
+    }
+    
     func loadAppearance() {
         weightText.keyboardType = .decimalPad
         heightText.keyboardType = .decimalPad
         bmiResultLabel.text = nil
         bmiMessageLabel.text = nil
-        calculateButton.backgroundColor = UIColor.blue
+        calculateButton.backgroundColor = UIColor(red:0.96, green:0.54, blue:0.10, alpha:1.0)
         calculateButton.layer.cornerRadius = 5
         calculateButton.tintColor = UIColor.white
         loadTextMeasurement()
@@ -112,39 +142,7 @@ class ViewController: UIViewController {
             break;
         }
     }
-    
-    func getContextFromCoreData () -> NSManagedObjectContext {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context = appDelegate.persistentContainer.viewContext
-        return context
-    }
-    
-    func loadHistoryData() {
-        let context = getContextFromCoreData()
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BmiData")
-        request.returnsObjectsAsFaults = false
-        historyDataLabel.text! = ""
-        do {
-            let results = try context.fetch(request)
-            for index in stride(from: results.count-1, to: 0, by: -1) {
-                let item = results[index] as! NSManagedObject
-                if let createdDate = item.value(forKey: "createdDate") as? String {
-                    historyDataLabel.text! += createdDate + ": "
-                }
-                if let result = item.value(forKey: "result") as? String {
-                    historyDataLabel.text! += result + ". "
-                }
-                if let message = item.value(forKey: "message") as? String {
-                    historyDataLabel.text! += message + "."
-                }
-                historyDataLabel.text! += "\n"
-            }
-        } catch {
-            print("Request failed")
-            print("Unexpected error: \(error).")
-        }
-    }
-    
+        
     func saveToHistoryData() {
         do {
             let context = getContextFromCoreData()
@@ -153,12 +151,39 @@ class ViewController: UIViewController {
             newValue.setValue(getNow(), forKey: "createdDate")
             newValue.setValue(bmiResultLabel.text, forKey: "result")
             newValue.setValue(bmiMessageLabel.text, forKey: "message")
+            
+            var historyDataList = fetchHistoryDataList()
+            if historyDataList.count > historyDataMaxCount {
+                context.delete(historyDataList[historyDataList.count-1] )
+                historyDataList.remove(at: historyDataList.count-1)
+            }
         
             try context.save()
         } catch {
             print("Failed to save")
             print("Unexpected error: \(error).")
         }
+    }
+    
+    func getContextFromCoreData () -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        return context
+    }
+    
+    func fetchHistoryDataList() -> [NSManagedObject] {
+        let context = getContextFromCoreData()
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "BmiData")
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(request)
+            return results as! [NSManagedObject]
+        } catch {
+            print("Request failed")
+            print("Unexpected error: \(error).")
+        }
+        
+        return [NSManagedObject]()
     }
     
     func getNow() -> String {
